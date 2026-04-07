ee83d582bb2723fc2e417d02c1ade92f08c493fa
mushrifa-hussain <mushrifa.hussain2005@gmail.com>
2026-04-08 00:14:05 +0530

    Add files via upload

---
+++ models/blogModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+
+  content: {
+    type: String,
+    required: true
+  },
+
+  author: {
+    type: String,
+    required: true
+  },
+
+  tags: {
+    type: [String]
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
